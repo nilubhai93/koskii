f3e4e611ed2fc97e20ba70b05d95d1f33d4cfb59
nilubhai93 <niladrimaji9382@gmail.com>
2025-07-09 15:49:03 +0530

    shopping

--- script.js
+++ script.js
@@ -222,3 +222,49 @@ productImages.forEach(img => {
 });
 
 console.log('Koskii website loaded successfully!');
+
+
+
+
+
+
+
+
+   const heroSection = document.getElementById("heroSection");
+
+  const images = [
+      "images/slide1.png",
+      "images/slide2.png",
+      "images/slide3.png",
+      "images/slide4.png"
+  ];
+
+  let currentSlide = 0;
+  let autoSlideInterval = null;
+
+  function setBackgroundImage(index) {
+      heroSection.style.backgroundImage = `url('${images[index]}')`;
+  }
+
+  function changeSlide(direction) {
+      currentSlide = (currentSlide + direction + images.length) % images.length;
+      setBackgroundImage(currentSlide);
+      resetAutoSlide();
+  }
+
+  function autoSlide() {
+      currentSlide = (currentSlide + 1) % images.length;
+      setBackgroundImage(currentSlide);
+  }
+
+  function resetAutoSlide() {
+      clearInterval(autoSlideInterval);
+      autoSlideInterval = setInterval(autoSlide, 2000);
+  }
+
+  // Start after DOM is ready
+  window.addEventListener("DOMContentLoaded", () => {
+      setBackgroundImage(currentSlide);
+      autoSlideInterval = setInterval(autoSlide, 3000);
+  });
+
